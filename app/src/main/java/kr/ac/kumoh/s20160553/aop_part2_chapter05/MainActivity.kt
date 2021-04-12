6d7265e2b2589a9fc60b88bff9c177c46410dc7c
20160553 <53904156+20160553@users.noreply.github.com>
2021-04-12 16:41:30 +0900

    이미지 액자에 넣기 구현

--- app/src/main/java/kr/ac/kumoh/s20160553/aop_part2_chapter05/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20160553/aop_part2_chapter05/MainActivity.kt
@@ -64,6 +64,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun initStartPhotoFrameModeButton() {
+        startPhotoFrameModeButton.setOnClickListener {
+            val intent = Intent(this, PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed{index, uri ->
+                intent.putExtra("photo$index", uri.toString())
+            }
+            intent.putExtra("photoListSize", imageUriList.size)
+        }
+    }
+
     private fun navigatePhotos() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
@@ -129,10 +139,6 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButton("취소하기", {_, _ -> })
             .create()
             .show()
-    }
-
-    private fun initStartPhotoFrameModeButton() {
-
     }
 
 }
