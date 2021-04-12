135e071a3ad4566c62497b6c6b6bc8ee12b6f370
20160553 <53904156+20160553@users.noreply.github.com>
2021-04-12 16:04:29 +0900

    이미지 액자에 넣기 구현

--- app/src/main/java/kr/ac/kumoh/s20160553/aop_part2_chapter05/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20160553/aop_part2_chapter05/MainActivity.kt
@@ -1,9 +1,14 @@
 package kr.ac.kumoh.s20160553.aop_part2_chapter05
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 
@@ -16,6 +21,19 @@ class MainActivity : AppCompatActivity() {
     val startPhotoFrameModeButton: Button by lazy{
         findViewById<Button>(R.id.startPhotoFrameModeButton)
     }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById<ImageView>(R.id.imageView1_1))
+            add(findViewById<ImageView>(R.id.imageView1_2))
+            add(findViewById<ImageView>(R.id.imageView1_3))
+            add(findViewById<ImageView>(R.id.imageView2_1))
+            add(findViewById<ImageView>(R.id.imageView2_2))
+            add(findViewById<ImageView>(R.id.imageView2_3))
+        }
+    }
+
+    private val imageUriList: MutableList<Uri> =  mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +50,8 @@ class MainActivity : AppCompatActivity() {
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
-                    //todo 권한이 잘 부여되었을 때 갤러리에서 사진 선택하는 기능
+                    //권한이 잘 부여되었을 때 갤러리에서 사진 선택하는 기능
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     // 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
@@ -45,8 +64,59 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initStartPhotoFrameModeButton() {
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when(requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null) {
+                    if(imageUriList.size > 6) {
+                        Toast.makeText(this, "이미 사진이 꽉 찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            } else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //권한이 부여된 것이빈다.
+                    navigatePhotos()
+
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            } else -> {
+                //
+            }
+        }
     }
 
     private fun showPermissionContextPopup() {
@@ -60,4 +130,9 @@ class MainActivity : AppCompatActivity() {
             .create()
             .show()
     }
+
+    private fun initStartPhotoFrameModeButton() {
+
+    }
+
 }
